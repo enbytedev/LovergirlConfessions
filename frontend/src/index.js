ebd1a0f9c141e80033873ae858450182d0284b8c
enbytedev <me@enbyte.dev>
2024-09-19 09:40:27 -0700

    Implemented analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize("G-D9TRX6MC4W");
+ReactGA.send("pageview");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
